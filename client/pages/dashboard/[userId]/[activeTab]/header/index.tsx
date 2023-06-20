af92cb973636266af4c660ef75f7c673c755ea81
basia-borkowska <barbara.borkowska@htdevelopers.com>
2023-06-20 16:12:14 +0200

    Remove unnecessary return

--- client/pages/dashboard/[userId]/[activeTab]/header/index.tsx
+++ client/pages/dashboard/[userId]/[activeTab]/header/index.tsx
@@ -1,21 +1,17 @@
 import { AvatarComponent } from '@/components/avatar'
 import { ButtonComponent } from '@/components/button'
-import { Container } from '@mantine/core'
 
 interface HeaderProps {
   userName: string
   userAvatar?: string
+
   openModal: () => void
 }
-const HeaderComponent = ({ userName, userAvatar, openModal }: HeaderProps) => {
-  return (
-    <div className=" flex items-center justify-between mb-8">
-      <AvatarComponent src={userAvatar} name={userName} compact />
-      <ButtonComponent className="basis-2/6" variant="outline" onClick={openModal}>
-        Add new measurement
-      </ButtonComponent>
-    </div>
-  )
-}
-
-export default HeaderComponent
+export const HeaderComponent = ({ userName, userAvatar, openModal }: HeaderProps) => (
+  <div className=" flex items-center justify-between mb-8">
+    <AvatarComponent src={userAvatar} name={userName} compact />
+    <ButtonComponent className="basis-2/6" variant="outline" onClick={openModal}>
+      Add new measurement
+    </ButtonComponent>
+  </div>
+)
