d01aeca62cc82859807a3b0c6357a427444a56f1
mikolaj-marcoll <mikolaj.marcoll@htdevelopers.com>
2023-06-21 17:17:01 +0200

    small changes to area chart

--- client/components/chart/index.tsx
+++ client/components/chart/index.tsx
@@ -1,50 +1,33 @@
 import { ChartDataType } from '@/types/ChartData'
 import { useMantineTheme } from '@mantine/core'
-import { ReactNode } from 'react'
-import { CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, AreaChart, Area } from 'recharts'
-import { CurveType } from 'recharts/types/shape/Curve'
+import { XAxis, YAxis, Tooltip, ResponsiveContainer, AreaChart, Area } from 'recharts'
 import { TooltipComponent } from './tooltip'
 
 interface ChartProps {
   data: ChartDataType[]
-  type?: CurveType
-  lineStroke?: string
-
-  withGrid?: boolean
-  customTooltip?: ReactNode
-  withLegend?: boolean
+  color?: string
 }
 
-export const ChartComponent = ({
-  data,
-  withGrid = false,
-  customTooltip,
-  withLegend = false,
-
-  type = 'monotone',
-  lineStroke,
-}: ChartProps) => {
+export const ChartComponent = ({ data, color }: ChartProps) => {
   const { colors } = useMantineTheme()
-  const color = lineStroke || colors['blue-100'][0]
+  const areaColor = color || colors['blue-100'][0]
 
   return (
-    <ResponsiveContainer width={600} height={300}>
-      <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+    <ResponsiveContainer width="60%" height={400}>
+      <AreaChart data={data} margin={{ top: 20 }}>
         <defs>
           <linearGradient id="yAxis" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor={color} stopOpacity={0.8} />
-            <stop offset="95%" stopColor={color} stopOpacity={0} />
+            <stop offset="5%" stopColor={areaColor} stopOpacity={0.8} />
+            <stop offset="95%" stopColor={areaColor} stopOpacity={0} />
           </linearGradient>
         </defs>
         <XAxis dataKey="xAxis" />
         <YAxis />
-        {withGrid && <CartesianGrid strokeDasharray="3 3" />}
         <Tooltip labelFormatter={(value) => value} content={<TooltipComponent />} cursor />
-        {withLegend && <Legend />}
         <Area
-          type={type}
+          type="monotone"
           dataKey="yAxis.value"
-          stroke={color}
+          stroke={areaColor}
           strokeWidth="3"
           fillOpacity="1"
           fill="url(#yAxis)"
