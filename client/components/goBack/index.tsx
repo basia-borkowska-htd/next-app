f7f0850be8086000e097479334cb7f51aa253ebf
mikolaj-marcoll <mikolaj.marcoll@htdevelopers.com>
2023-06-21 11:57:07 +0200

    add link component

--- client/components/goBack/index.tsx
+++ client/components/goBack/index.tsx
@@ -1,16 +1,16 @@
-import { useRouter } from 'next/router'
-import { ButtonComponent } from '../button'
+import Link from 'next/link'
 import { IconArrowNarrowLeft } from '@tabler/icons-react'
+
+import { ButtonComponent } from '../button'
+
 interface GoBackProps {
   path: string
 }
 
-export const GoBackComponent = ({ path }: GoBackProps) => {
-  const router = useRouter()
-
-  return (
-    <ButtonComponent className="mb-3 ps-0" onClick={() => router.push(path)} variant="icon" fullWidth={false}>
+export const GoBackComponent = ({ path }: GoBackProps) => (
+  <Link href={path}>
+    <ButtonComponent className="mb-3 ps-0" variant="icon" fullWidth={false}>
       <IconArrowNarrowLeft size={32} />
     </ButtonComponent>
-  )
-}
+  </Link>
+)
