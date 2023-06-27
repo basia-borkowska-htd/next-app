3a99aeb0090e268fa802065a3ee0fcf729388964
basia-borkowska <barbara.borkowska@htdevelopers.com>
2023-06-27 10:18:15 +0200

    cr

--- client/pages/dashboard/[userId]/[activeTab]/chartsTab/chartItem/index.tsx
+++ client/pages/dashboard/[userId]/[activeTab]/chartsTab/chartItem/index.tsx
@@ -3,7 +3,9 @@ import { useQuery } from '@tanstack/react-query'
 import { api } from '@/api'
 import { ChartComponent } from '@/components/chart'
 import { MeasurementEnum, MeasurementLabels } from '@/enums/Measurement.enum'
-import { PanelComponent } from '@/components/panel'
+import { PageLoaderComponent } from '@/components/pageLoader'
+import { ErrorComponent } from '@/components/error'
+import { EmptyStateComponent } from '@/components/emptyState'
 
 interface ChartItemProps {
   userId: string
@@ -21,22 +23,21 @@ export const ChartItemComponent = ({ userId, itemKey }: ChartItemProps) => {
     queryFn: () => api.measurement.getChartMeasurements(userId, itemKey),
   })
 
+  const getPanelComponent = () => {
+    if (isLoading || isFetching) return <PageLoaderComponent compact />
+    if (isError) return <ErrorComponent compact />
+    if (!chart) return <EmptyStateComponent title={`No ${title} Measurements`} compact />
+
+    return <ChartComponent data={chart} />
+  }
+
   return (
     <Accordion.Item value={itemKey}>
       <Accordion.Control>
         <div className="font-bold">{title}</div>
       </Accordion.Control>
       <Accordion.Panel>
-        <div className="flex justify-center">
-          <PanelComponent
-            isLoading={isFetching || isLoading}
-            error={isError}
-            invalidData={!chart?.length}
-            invalidDataMessage={`No ${title} Measurements`}
-          >
-            <ChartComponent data={chart} />
-          </PanelComponent>
-        </div>
+        <div className="flex justify-center">{getPanelComponent()}</div>
       </Accordion.Panel>
     </Accordion.Item>
   )
