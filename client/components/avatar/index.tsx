a6ebc953b3b5829d924a70291e111195787bfe93
basia-borkowska <barbara.borkowska@htdevelopers.com>
2023-07-03 13:57:05 +0200

    cr

--- client/components/avatar/index.tsx
+++ client/components/avatar/index.tsx
@@ -15,18 +15,16 @@ export const AvatarComponent = ({ src, name, compact = false, className = '', ce
       compact ? 'flex-row gap-5' : 'flex-col gap-2'
     } ${className}`}
   >
-    <div className="relative">
-      <Avatar
-        src={src}
-        alt="avatar"
-        variant="filled"
-        color="green-300"
-        radius={compact ? 'xl' : 'lg'}
-        size={compact ? 'lg' : 'xl'}
-      >
-        <IconUser size={compact ? '30' : '60'} />
-      </Avatar>
-    </div>
+    <Avatar
+      src={src}
+      alt="avatar"
+      variant="filled"
+      color="green-300"
+      radius={compact ? 'xl' : 'lg'}
+      size={compact ? 'lg' : 'xl'}
+    >
+      <IconUser size={compact ? '30' : '60'} />
+    </Avatar>
 
     {!!name && <div className={compact ? 'text-3xl' : 'text-xl font-bold'}>{name}</div>}
   </div>
