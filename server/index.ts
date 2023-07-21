db499eeafd6f417ab31ec415523711a3b850f79f
basia-borkowska <barbara.borkowska@htdevelopers.com>
2023-07-21 09:52:39 +0200

    Revert "heroku config"
    This reverts commit 4422dff003680410c7bbe8c20b52064df8154dd3.

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ const app = express()
 
 mongoose
   .connect(process.env.MONGO_URI || '')
-  .then(() => app.listen(process.env.PORT || 3001))
+  .then(() => app.listen(process.env.PORT))
   .catch((err) => console.log(err))
 
 app.use(cors())
