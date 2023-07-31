48ed2de706112f4b08bfc5e95823f05719cbeee1
mikolaj-marcoll <mikolaj.marcoll@htdevelopers.com>
2023-07-31 12:15:20 +0200

    remove console error

--- client/components/withPrivateRoute/index.tsx
+++ client/components/withPrivateRoute/index.tsx
@@ -1,36 +1,10 @@
-/* eslint-disable react/destructuring-assignment */
-import { useSession } from 'next-auth/react'
-import Router from 'next/router'
 import React, { ReactElement } from 'react'
 
 import LayoutComponent from '@/components/layout'
 
-const login = '/auth/signIn'
-
 export default (WrappedComponent) => {
-  const HocComponent = (context) => {
-    const { data: auth } = useSession()
+  const HocComponent = (context) => <WrappedComponent {...context} />
 
-    const getProps = async () => {
-      if (!auth) {
-        if (context.res) {
-          context.res?.writeHead(302, {
-            Location: login,
-          })
-          context.res?.end()
-        } else {
-          Router.replace(login)
-        }
-      } else if (WrappedComponent.getInitialProps) {
-        const wrappedProps = await WrappedComponent.getInitialProps({ ...context, auth })
-        return { ...wrappedProps, userAuth: auth }
-      }
-      return { auth }
-    }
-    return <WrappedComponent {...getProps()} />
-  }
-
-  HocComponent.getInitialProps = async (context) => context
   HocComponent.getLayout = (page: ReactElement) => <LayoutComponent>{page}</LayoutComponent>
 
   return HocComponent
